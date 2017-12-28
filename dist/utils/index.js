e02a79c5afe6157b1931982da5bd1f003248c8be
huangzilong <huangzilongg@gmail.com>
2017-12-28 22:21:02 +0800

    add dist

--- dist/utils/index.js
+++ dist/utils/index.js
@@ -1,13 +1,25 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 var path = require('path');
 var fs = require('fs');
-function getVersion(params) {
-    var filepath = path.resolve(__dirname, '../package.json');
-    var version = JSON.parse(fs.readFile(filepath)).version;
-    return version;
-}
 
 module.exports = {
-    getVersion: getVersion
+    readFile: function readFile(p) {
+        var rst = '';
+        p = (typeof p === 'undefined' ? 'undefined' : _typeof(p)) === 'object' ? path.join(p.dir, p.base) : p;
+        try {
+            rst = fs.readFileSync(p, 'utf-8');
+        } catch (e) {
+            rst = null;
+        }
+        return rst;
+    },
+
+    getVersion: function getVersion(params) {
+        var filepath = path.resolve(__dirname, '../../package.json');
+        var version = JSON.parse(this.readFile(filepath)).version;
+        return version;
+    }
 };
