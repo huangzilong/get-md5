ffc07a61ebbcf4366a16265a94ee23fc132eb485
huangzilong <huangzilongg@gmail.com>
2017-12-28 22:20:18 +0800

    add readfile

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const fs = require('fs');
-function getVersion(params) {
-    const filepath = path.resolve(__dirname, '../package.json');
-    const version = JSON.parse(fs.readFile(filepath)).version;
-    return version;
-}
 
 module.exports = {
-    getVersion,
-}
+    readFile(p) {
+        let rst = '';
+        p = typeof p === 'object' ? path.join(p.dir, p.base) : p;
+        try {
+            rst = fs.readFileSync(p, 'utf-8');
+        } catch (e) {
+            rst = null;
+        }
+        return rst;
+    },
+    getVersion: function(params) {
+        const filepath = path.resolve(__dirname, '../../package.json');
+        const version = JSON.parse(this.readFile(filepath)).version;
+        return version;
+    }
+};
