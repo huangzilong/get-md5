c79af9bb6d662276045e48e41025d609f2045382
huangzilong <huangzilongg@gmail.com>
2017-12-28 18:23:10 +0800

    add module path

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 function getVersion(params) {
     const filepath = path.resolve(__dirname, '../package.json');
     const version = JSON.parse(this.readFile(filepath)).version;
