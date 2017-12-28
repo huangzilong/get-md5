e78caaa90e42e017e6a09a775b46a261880dcfce
huangzilong <huangzilongg@gmail.com>
2017-12-28 19:53:34 +0800

    add fs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const fs = require('fs');
 function getVersion(params) {
     const filepath = path.resolve(__dirname, '../package.json');
-    const version = JSON.parse(this.readFile(filepath)).version;
+    const version = JSON.parse(fs.readFile(filepath)).version;
     return version;
 }
 
