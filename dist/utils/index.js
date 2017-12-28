d49b4a4726c4bf9e5eeddf38009852c8aee35fa8
huangzilong <huangzilongg@gmail.com>
2017-12-28 18:13:44 +0800

    add ist

---
+++ dist/utils/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function getVersion(params) {
+    var filepath = path.resolve(__dirname, '../package.json');
+    var version = JSON.parse(this.readFile(filepath)).version;
+    return version;
+}
+
+module.exports = {
+    getVersion: getVersion
+};
