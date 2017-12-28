5f4d8a5c8e0d5b3e8b6efd2f4e2a3adf27df6f23
huangzilong <huangzilongg@gmail.com>
2017-12-28 19:47:42 +0800

    add built files

--- dist/utils/index.js
+++ dist/utils/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var path = require('path');
 function getVersion(params) {
     var filepath = path.resolve(__dirname, '../package.json');
     var version = JSON.parse(this.readFile(filepath)).version;
